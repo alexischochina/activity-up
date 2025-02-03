87bbb0f503e14bd6bfb6c56be27bb85e936874d1
Ayoub <ayoub.ouahid@unify-technologies.com>
2025-02-03 14:54:56 +0100

    detabase setup

---
+++ main.session.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS users (
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  pwd VARCHAR(255) NOT NULL,
+  registration DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS type_activite (
+  activitie_name VARCHAR(255) NOT NULL,
+);
+CREATE TABLE IF NOT EXISTS activites (
+  activite_name VARCHAR(255) NOT NULL,
+  type_id REFERENCES type_activite(ROWID),
+  places_disponibles INT NOT NULL,
+  activite_description VARCHAR(255) NOT NULL,
+  datetime_debut DATETIME NOT NULL,
+  durée INT NOT NULL,
+);
+CREATE TABLE IF NOT EXISTS reservations (
+  user_id REFERENCES users(ROWID),
+  activitie_id REFERENCES activites(ROWID),
+  date_reservation DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+  etat BOOLEAN NOT NULL DEFAULT TRUE
+); 
